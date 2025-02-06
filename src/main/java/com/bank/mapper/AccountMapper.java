f8a412822466016fa213c164c86c76fe1614cc9d
itroqkfwk <wjdeo3321@gmail.com>
2025-02-06 17:30:01 +0900

    Step#2

--- src/main/java/com/bank/mapper/AccountMapper.java
+++ src/main/java/com/bank/mapper/AccountMapper.java
@@ -8,12 +8,9 @@ import org.apache.ibatis.annotations.Select;
 
 import com.bank.dto.AccountDTO;
 
-@Mapper
+
 public interface AccountMapper {
 
-    @Select("SELECT * FROM Accounts")
-    List<AccountDTO> selectAllAccounts();
-
-    @Select("SELECT * FROM Accounts WHERE id = #{id}")
-    AccountDTO selectAccountById(@Param("id") long id);
+	List<AccountDTO> selectAllAccounts();
+    AccountDTO selectAccountById(int id);
 }
